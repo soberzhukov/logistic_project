b04c8c6e09bc7cbfdfd56735b9d59dbbce90b172
soberzhukov <soberzhukov@mail.ru>
2022-11-22 01:32:21 +0300

    fix

--- users/admin.py
+++ users/admin.py
@@ -5,13 +5,18 @@ from users.models import User, ConfirmPassword, ConfirmPhone
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'first_name', 'last_name', 'patronymic', 'email', 'push_token', 'is_blocked',
+    list_display = ['id', 'username', 'first_name', 'last_name', 'patronymic', 'email', 'push_token', 'is_blocked',
                     'cause_blocked', 'is_admin']
 
     def save_model(self, request, obj, form, change):
-        old_user = User.objects.get(pk=obj.pk)
-        if old_user.password != obj.password:
+
+        if change:
+            old_user = User.objects.get(pk=obj.pk)
+            if old_user.password != obj.password:
+                obj.set_password(obj.password)
+        else:
             obj.set_password(obj.password)
+
         obj.save()
 
 
