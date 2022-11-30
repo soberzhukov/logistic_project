d8360ef483450d223efb29fc2250d5b2ae927c66
soberzhukov <soberzhukov@mail.ru>
2022-11-30 10:46:38 +0300

    admin search

--- order/admin.py
+++ order/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from order.models import Order
+from order.models import Order, SavedSearch
 
 
 @admin.register(Order)
@@ -8,3 +8,6 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'author', 'name', 'date_created']
 
 
+@admin.register(SavedSearch)
+class SavedSearchAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'type_search', 'date_create']
